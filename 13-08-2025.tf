938f9252a268e30848aa5c784ac3a9d73d3cce7a
Brihatthi1931 <brihatthi19@gmail.com>
2025-08-13 14:35:11 +0530

    new commit

--- 13-08-2025.tf
+++ 13-08-2025.tf
@@ -8,10 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
+    features {}
+    subscription_id = "381b371f-c06d-4487-a677-8d1e6f61613f"
   # Configuration options
 }
 
-resource "azurerm_resource_group" "13-08-2025" {
+resource "azurerm_resource_group" "Today" {
   name     = "13-08-2025"
   location = "West Europe"
 }
